d6a05cc692cf184295be1f1dbab3827b3066fa20
Sheng Xuan <shengxuan14@gmail.com>
2018-10-17 23:38:02 +0800

    add message service for inter component communication

---
+++ webapp/src/app/services/message.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  private subject = new Subject<any>();
+  sendMessage(message: string) {
+    this.subject.next({message: message});
+  }
+  getMessage(): Observable<any> {
+    return this.subject.asObservable();
+  }
+}
