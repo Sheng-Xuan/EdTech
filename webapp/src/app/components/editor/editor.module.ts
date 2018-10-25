02b764025ed941760c56b7eecb061f69474049ac
Sheng Xuan <shengxuan14@gmail.com>
2018-10-25 12:57:11 +0800

    remove old editor

--- webapp/src/app/components/editor/editor.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { EditorComponent } from './editor.component';
-
-@NgModule({
-    imports: [],
-    declarations: [EditorComponent],
-})
-
-export class EditorModule {}
