308ad2205add28031d9810375d118f54243d7276
Sheng Xuan <shengxuan14@gmail.com>
2018-10-25 12:57:24 +0800

    update gulpfile

--- backend/gulpfile.js
+++ backend/gulpfile.js
@@ -1,23 +1,42 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+let exec = require('child_process').exec;
 
 // pull in the project TypeScript config
 const tsProject = ts.createProject('tsconfig.json');
+const PATHS = {
+  scripts: ['./src/**/*.ts'],
+  output: './dest'
+};
+let child;
 
-gulp.task('scripts', () => {
-  const tsResult = tsProject.src()
-  .pipe(tsProject());
-  return tsResult.js.pipe(gulp.dest('dist'));
+gulp.task('restart', function() {
+  child = exec(
+    'supervisor -w build ./dest/index.js',
+    (error, stdout, stderr) => {
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+      if (error !== null) {
+        console.log(`exec error: ${error}`);
+      }
+    }
+  );
 });
 
-gulp.task('watch', ['scripts'], () => {
-  gulp.watch('src/**/*.ts', ['scripts']);
+gulp.task('build', gulp.series(['restart']), function() {
+  return gulp
+    .src(PATHS.scripts)
+    .pipe(tsProject())
+    .pipe(gulp.dest(PATHS.output));
+});
+
+gulp.task('watch', gulp.series(['build']), function() {
+  gulp.watch(PATHS.scripts, ['build']);
 });
 
 gulp.task('assets', function() {
-  return gulp.src(JSON_FILES)
-  .pipe(gulp.dest('dist'));
+  return gulp.src(JSON_FILES).pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['watch', 'assets']);
+gulp.task('dev', gulp.series('build', 'assets', 'restart', 'watch'));
