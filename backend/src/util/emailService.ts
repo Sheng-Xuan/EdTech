bb7e8486dd5ede32fabfd446bd2422a777179de9
Sheng Xuan <shengxuan14@gmail.com>
2019-03-01 20:25:22 +0800

    format

--- backend/src/util/emailService.ts
+++ backend/src/util/emailService.ts
@@ -4,16 +4,15 @@ import sgMail = require('@sendgrid/mail');
 
 const senderAddress = 'noreply@edtech.top';
 
-export function sendUserVerification(userEmail: string, code: string, username: string) {
-  sendEmail(
-    userEmail,
-    senderAddress,
-    process.env.SENDGRID_VERIFICATION_ID,
-    {
-        url: process.env.URL + '/verification?code=' + code + '&email=' + userEmail,
-        username: username
-    }
-  );
+export function sendUserVerification(
+  userEmail: string,
+  code: string,
+  username: string
+) {
+  sendEmail(userEmail, senderAddress, process.env.SENDGRID_VERIFICATION_ID, {
+    url: process.env.URL + '/verification?code=' + code + '&email=' + userEmail,
+    username: username
+  });
 }
 export function sendForgotPassword(userEmail: string, code: string) {
   sendEmail(userEmail, senderAddress, process.env.SENDGRID_FORGET_PASSWORD, {
@@ -21,12 +20,7 @@ export function sendForgotPassword(userEmail: string, code: string) {
   });
 }
 
-function sendEmail(
-  to: string,
-  from: string,
-  templateId: string,
-  data: object
-) {
+function sendEmail(to: string, from: string, templateId: string, data: object) {
   const msg = {
     to: to,
     from: from,
