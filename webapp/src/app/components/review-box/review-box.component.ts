5b0a4deda05652a925f1fbb5e2fab233fa41aa40
Sheng Xuan <shengxuan14@gmail.com>
2019-03-01 20:21:38 +0800

    add review box (card) component

---
+++ webapp/src/app/components/review-box/review-box.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-review-box',
+  templateUrl: './review-box.component.html',
+  styleUrls: ['./review-box.component.css']
+})
+export class ReviewBoxComponent implements OnInit {
+
+  @Input()
+  data: object;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getImagePath(review): string {
+    if (review && review.images.length > 0) {
+      return '/files/' + review.images[0].localFileName;
+    } else if (review.tool.images.length > 0) {
+      return '/files/' + review.tool.images[0].localFileName;
+    } else {
+      return 'assets/img-not-available.jpg';
+    }
+  }
+
+}
