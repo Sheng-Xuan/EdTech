55eadbb8c55302d5042b6e498481ff64bcdec355
Sheng Xuan <shengxuan14@gmail.com>
2019-03-07 23:58:15 +0800

    add comments

--- webapp/src/app/services/message.service.ts
+++ webapp/src/app/services/message.service.ts
@@ -6,9 +6,11 @@ import { Subject, Observable } from 'rxjs';
 })
 export class MessageService {
   private subject = new Subject<any>();
+  // To send message to trigger actions
   sendMessage(message: string) {
     this.subject.next({message: message});
   }
+  // To subscribe this service and get messages sent from other components
   getMessage(): Observable<any> {
     return this.subject.asObservable();
   }
